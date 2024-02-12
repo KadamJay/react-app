bb74c855d116e0dab2199aff34d9b80fdee0552c
KadamJay <investinself123@gmail.com>
2024-02-13 02:43:27 +0530

    passing children to the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import ListGroup from "./Components/ListGroup";
+import Alert from "./Components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading={"Cities"}
-        onSelection={handleSelectItem}
-      />
+      <Alert>
+        Hello <span> World!</span>
+      </Alert>
     </div>
   );
 }
